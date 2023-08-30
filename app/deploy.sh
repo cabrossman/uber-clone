d234a5ef417d2bf4c9e67e294661b25586f175b4
christopher brossman <cabrossman1@gmail.com>
2023-08-30 10:25:47 -0700

    fixed deploy.sh script

--- app/deploy.sh
+++ app/deploy.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-
-COMMAND="screen -S deployment /home/christopher.brossman/uber-clone/app/prod_deploy.sh"
-COMMAND="screen -dmS deployment && screen -S deployment -X stuff 'bash /home/christopher.brossman/uber-clone/app/prod_deploy.sh'"
-COMMAND="screen -dmS deployment bash -c '/home/christopher.brossman/uber-clone/app/prod_deploy.sh && bash'"
-
-
 gcloud compute ssh $INSTANCE_NAME \
     --zone $ZONE \
     --project $PROJECT_ID \
-    --command $COMMAND
+    --command "bash \$HOME/uber-clone/app/prod_deploy.sh"
