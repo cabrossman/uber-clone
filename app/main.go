13037a65577ba1004d24025a0725b2abab19fe59
christopher brossman <cabrossman1@gmail.com>
2023-09-01 16:05:05 -0700

    added docker compose

--- app/main.go
+++ app/main.go
@@ -1,19 +1,45 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "os"
+	db "app/postgres"
+	"fmt"
+	"net/http"
+	"os"
 )
 
-func getData(w http.ResponseWriter, req *http.Request) {
-  fmt.Fprintf(w, "Hello world\n")
+func getDrivers(w http.ResponseWriter, req *http.Request) {
+	rows, err := db.Connection.Query("SELECT name FROM drivers")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer rows.Close()
+
+	data := ""
+	for rows.Next() {
+		var name string
+		err = rows.Scan(&name)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(name)
+		data += fmt.Sprintf("%s ", name)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Fprintf(w, data)
 }
 
 func main() {
+	db.InitDB()
+	defer db.Connection.Close()
+
 	http.Handle("/", http.FileServer(http.Dir("./static")))
-	http.HandleFunc("/data", getData);
-	
+	http.HandleFunc("/drivers", getDrivers)
+
 	serverEnv := os.Getenv("SERVER_ENV")
 	if serverEnv == "" {
 		fmt.Println("Error: SERVER_ENV environment variable is not set.")
@@ -26,7 +52,7 @@ func main() {
 
 	} else if serverEnv == "PROD" {
 		fmt.Println("Using PROD Port")
-    	http.ListenAndServe(":80", nil)
+		http.ListenAndServe(":80", nil)
 
 	}
 }
