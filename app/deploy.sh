d16155a0bacb5d0e17dcc35029fa29cb22a78f6d
christopher brossman <cabrossman1@gmail.com>
2023-09-01 21:14:54 -0700

    added react

--- app/deploy.sh
+++ app/deploy.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+#build
+cd frontend
+npm run build
+cd ..
+
+#push to git
+git add .
+git commit -m "build"
+git push
+
+#deploy to prod
 gcloud compute ssh $INSTANCE_NAME \
     --zone $ZONE \
     --project $PROJECT_ID \
