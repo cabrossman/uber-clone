93225058546cd0667808e16176108b67f44f98c4
christopher brossman <cabrossman1@gmail.com>
2023-09-07 12:31:55 -0400

    added some more - server generated

--- operate/helpful_operate_commands.sh
+++ operate/helpful_operate_commands.sh
@@ -1,26 +1,27 @@
 source .env
 
-### SSH
-gcloud compute ssh $INSTANCE_NAME --zone $ZONE --project $PROJECT_ID
+gcloud compute instances start $INSTANCE_NAME --zone=$ZONE #start
+gcloud compute ssh $INSTANCE_NAME --zone $ZONE --project $PROJECT_ID #ssh
+gcloud compute instances stop $INSTANCE_NAME --zone=$ZONE #stop
+
+#build services
+git pull #get latest branch
+sudo docker build --tag app . #build docker filer
+sudo docker compose down #stop containers
+sudo docker compose up -d #start containers
+sudo docker image prune -f #cleanup
+
 
 ### COPY FILES
 #gcloud compute scp [LOCAL_FILE_PATH] [INSTANCE_NAME]:[REMOTE_FILE_PATH]
-gcloud compute scp --recurse $LOCAL_FILE_PATH/app/ $INSTANCE_NAME:~ --zone $ZONE
+#gcloud compute scp --recurse $LOCAL_FILE_PATH/app/ $INSTANCE_NAME:~ --zone $ZONE
 
-
-### START & STOP SERVER
-#start
-gcloud compute instances start $INSTANCE_NAME --zone=$ZONE
-#stop
-gcloud compute instances stop $INSTANCE_NAME --zone=$ZONE
-
-
-### START APP
+### START APP - Intermediate before docker ###
 #sudo go run app/main.go &
 #version below passes ENV VARS to sudo
-cd $HOME/uber-clone/app
-sudo go clean -cache
-sudo -E go run main.go &
+#cd $HOME/uber-clone/app
+#sudo go clean -cache
+#sudo -E go run main.go &
 # This actually starts 3 processes
 #...
 #2200302 pts/1    00:00:00 sudo
@@ -29,5 +30,6 @@ sudo -E go run main.go &
 #...
 
 #need to kill main
-ps aux | grep '[g]o'
-sudo pkill main
+#ps aux | grep '[g]o'
+#sudo pkill main
+
